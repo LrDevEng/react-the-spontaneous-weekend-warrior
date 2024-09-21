e6a88e87931deac890dd6b6aa33c489158ec486b
LrDevEng <lr.deveng@gmail.com>
2024-09-21 09:57:37 +0200

    Add standard html date and time picker (input elements)

---
+++ src/components/DateTimePicker.js
@@ -0,0 +1,10 @@
+function DateTimePicker() {
+  return (
+    <div>
+      <input type="date" value="2024-06-06" />
+      <input type="time" value="08:00" />
+    </div>
+  );
+}
+
+export default DateTimePicker;
